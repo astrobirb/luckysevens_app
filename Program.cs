5096209a7ebf7ca7ecff3fa7822a7bfe5893bdc5
astrobirby <nicolef10@learn.cssd.ab.ca>
2019-02-21 13:03:28 -0700

    DONE

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace app
 {
@@ -6,7 +10,23 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int totalNumberOfSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+
+                if (die1 + die2 == 7)
+                totalNumberOfSevens++;
+            }
+
+            Console.WriteLine("Out of 100 rolls, you rolled {0} sevens", totalNumberOfSevens);
+            Console.ReadLine();
         }
     }
 }
